7a0fbc0abc83eb61ea7e24431d5dd12b18c2fbb4
bobwad01-hue <bobwad01@gmail.com>
2026-09-03 17:01:04 -0500

    Add app layout

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'Rebels Recruit',description:'Your recruiting. Your relationships. Your next move.'}
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="en"><body>{children}</body></html>}
