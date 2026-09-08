54440f399c807a740ba0d14dfe69c160d8f7d6f0
bobwad01-hue <bobwad01@gmail.com>
2026-09-08 14:19:18 -0500

    Add Rebels team catalog

---
+++ lib/rebels-teams.ts
@@ -0,0 +1,11 @@
+export const REBELS_TEAMS=[
+'18 National Mayhugh',
+'18 Regional Jenkins',
+'16 National Olsen',
+'16 Regional Lickel',
+'16A Frans',
+'14 National Shafer',
+'14 Regional Oestmann',
+] as const;
+
+export type RebelsTeamName=typeof REBELS_TEAMS[number];
