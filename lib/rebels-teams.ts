6508236929b39829fc40f63790029817c44ef8e1
bobwad01-hue <bobwad01@gmail.com>
2026-09-08 16:40:42 -0500

    Prefix Rebels team names with KC Rebels

--- lib/rebels-teams.ts
+++ lib/rebels-teams.ts
@@ -1,11 +1,11 @@
 export const REBELS_TEAMS=[
-'18 National Mayhugh',
-'18 Regional Jenkins',
-'16 National Olsen',
-'16 Regional Lickel',
-'16A Frans',
-'14 National Shafer',
-'14 Regional Oestmann',
+'KC Rebels 18 National Mayhugh',
+'KC Rebels 18 Regional Jenkins',
+'KC Rebels 16 National Olsen',
+'KC Rebels 16 Regional Lickel',
+'KC Rebels 16A Frans',
+'KC Rebels 14 National Shafer',
+'KC Rebels 14 Regional Oestmann',
 ] as const;
 
 export type RebelsTeamName=typeof REBELS_TEAMS[number];
