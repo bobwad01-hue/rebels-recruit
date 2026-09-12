138d622425ac85fd510f08fe45ec06d2e0f99c41
bobwad01-hue <bobwad01@gmail.com>
2026-09-12 09:25:08 -0500

    Remove mismatched migration filename 20260912100000_restrict_staff_activity_feed_execution.sql

--- supabase/migrations/20260912100000_restrict_staff_activity_feed_execution.sql
+++
@@ -1,3 +0,0 @@
-revoke execute on function public.get_staff_recruiting_activity(uuid, integer) from public;
-revoke execute on function public.get_staff_recruiting_activity(uuid, integer) from anon;
-grant execute on function public.get_staff_recruiting_activity(uuid, integer) to authenticated;
